c2cba39d2d3a6fc1dd79e2709ebe03ae94dc2d70
Andrzej Perczak <linux@andrzejperczak.com>
2022-10-19 21:01:06 +0200

    f

--- BruteForceTSP.cpp
+++ BruteForceTSP.cpp
@@ -16,7 +16,7 @@ TSPResult BruteForceTSP::solve()
     do {
         size_t cur_weight = 0;
         cur_weight += matrix.at(0, perm[0] + 1);
-        for (size_t i = 1; i < matrix.get_cities_number() - 2; i++) {
+        for (size_t i = 1; i < matrix.get_cities_number() - 1; i++) {
             cur_weight += matrix.at(perm[i - 1] + 1, perm[i] + 1);
         }
         cur_weight += matrix.at(perm[matrix.get_cities_number() - 2] + 1, 0);
--- main.cpp
+++ main.cpp
@@ -10,17 +10,7 @@
 
 int main()
 {
-    //menu();
-    QuickPermArray perm(9);
-    int a = 0;
-    do {
-        for (int i = 0; i < 3; i++) {
-            std::cout << perm[i];
-        }
-        std::cout << std::endl;
-        a++;
-    } while (perm.next_permutation());
-    cout << a << std::endl;
+    menu();
 
     return 0;
 }
